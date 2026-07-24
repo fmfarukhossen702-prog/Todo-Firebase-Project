48cc5951d557b7fa6d1e9bffbc6c89d6a9c9c3d9
fmfarukhossen702 <fmfarukhossen702@gmail.com>
2026-07-24 16:01:24 +0600

    add line through

--- src/component/Todo.jsx
+++ src/component/Todo.jsx
@@ -12,6 +12,7 @@ import { FaTrashCan } from "react-icons/fa6";
 import { FaRegEdit } from "react-icons/fa";
 import { FaLocationArrow } from "react-icons/fa";
 import { FaArrowRotateLeft } from "react-icons/fa6";
+import { ToastContainer, toast, Bounce } from "react-toastify";
 
 // import image from '../assets/images.jpeg'
 const Todo = () => {
@@ -20,6 +21,49 @@ const Todo = () => {
   const [edit, setEdit] = useState(false);
   const [value, setValue] = useState("");
   const [idName, setIdName] = useState("");
+  const [line, setLine] = useState(false)
+
+  const notify = (type) => {
+    if (type === "success") {
+      toast.success("Your task has been successfully.", {
+        position: "top-right",
+        autoClose: 2500,
+        hideProgressBar: false,
+        closeOnClick: false,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+        transition: Bounce,
+      });
+    }
+    if (type === "delet") {
+      toast.success("Delet successfully.", {
+        position: "top-right",
+        autoClose: 2500,
+        hideProgressBar: false,
+        closeOnClick: false,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+        transition: Bounce,
+      });
+    }
+    if (type === "update") {
+      toast.success("Update seccessfully", {
+        position: "top-right",
+        autoClose: 2500,
+        hideProgressBar: false,
+        closeOnClick: false,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+        transition: Bounce,
+      });
+    }
+  };
 
   // const dataArr = Object.values(data || {});
 
@@ -31,7 +75,9 @@ const Todo = () => {
     const db = getDatabase();
     set(push(ref(db, "/todoName")), {
       todoName: task,
-    }).then(setTask(""));
+    }).then(() => {
+      (setTask(""), notify("success"));
+    });
   };
 
   useEffect(() => {
@@ -50,7 +96,7 @@ const Todo = () => {
 
   const handleDelete = (id) => {
     const db = getDatabase();
-    remove(ref(db, "/todoName/" + id));
+    remove(ref(db, "/todoName/" + id)).then(notify("delet"));
   };
   const handleEdit = (value, id) => {
     setEdit(!edit);
@@ -63,7 +109,7 @@ const Todo = () => {
     const db = getDatabase();
     update(ref(db, "/todoName/" + idName), {
       todoName: value,
-    });
+    }).then(notify("update"));
     setEdit(false);
     setValue("");
     setIdName("");
@@ -72,81 +118,94 @@ const Todo = () => {
     const db = getDatabase();
     remove(ref(db, "/todoName/"));
   };
+  const handleThrough = ()=> {
+    const db = getDatabase()
+    update(ref(db, "/todoName/" + idName))
+    setLine(!line)
+  }
 
   return (
-    <div className=" bg-[#d43d3d] text-white! bg-[url(./assets/images.jpeg)] bg-cover  pt-10  bg-center bg-no-repeat h-screen ">
-      <form className="  max-h-[90vh] fieldset bg-[#ffffff14] py-5 mx-auto   border-base-300 rounded-box w-full lg:w-lg border p-10 ">
-        <h1 className=" text-4xl text-center mb- ">Todo Application</h1>
-        <fieldset className="   fieldset">
-          <label className="label font-semibold text-lg mt-4 ">
-            Enter your task here
-          </label>
-          <input
-            onChange={(e) =>
-              edit ? setValue(e.target.value) : setTask(e.target.value)
-            }
-            type="text"
-            value={edit ? value : task}
-            className="input bg-[#ffffff42] w-full my-3"
-            placeholder="text"
-          />
-          {data.length > 0 && (
-            <div className=" flex justify-between items-center pr-5">
-              <div
-                onClick={handleAllData}
-                className="flex items-center gap-2 py-1 px-2 bg-[#000000] border border-[#ea1818] rounded-[5px] w-30 justify-center text-[#ca0a0a] text-[14px]  cursor-pointer"
-              >
-                All data <FaTrashCan />
-              </div>
-              <div>
-                {edit ? (
-                  <FaArrowRotateLeft
-                    className=" text-[#07c9ff] text-lg  cursor-pointer  "
-                    onClick={handleUpdate}
-                  />
-                ) : (
-                  <FaLocationArrow
-                    className=" text-[#07c9ff] text-lg  cursor-pointer "
-                    onClick={handleClick}
-                  />
-                )}
-              </div>
-            </div>
-          )}
-          <ul className=" max-h-[150px] overflow-y-auto ">
-            {data.map((item, index) => (
-              <li
-                key={index}
-                className="  w-full my-3 py-3 px-5  rounded-[5px] flex justify-between items-center text-lg bg-[#ffffff36] "
-              >
-                {item.value.todoName}
-
-                <div className="flex gap-3 items-center">
-                  <FaRegEdit
-                    onClick={() => handleEdit(item.value.todoName, item.id)}
-                    className="text-green-500 cursor-pointer "
-                  />
-
-                  <FaTrashCan
-                    onClick={() => handleDelete(item.id)}
-                    className=" cursor-pointer text-red-500 "
-                  />
+    <>
+      <ToastContainer />
+      <div className=" bg-[#d43d3d] text-white! bg-[url(./assets/images.jpeg)] bg-cover  pt-10  bg-center bg-no-repeat h-screen ">
+        <form className="  max-h-[90vh] fieldset bg-[#ffffff14] py-5 mx-auto   border-base-300 rounded-box w-full lg:w-lg border p-10 ">
+          <h1 className=" text-4xl text-center mb- ">Todo Application</h1>
+          <fieldset className="   fieldset">
+            <label className="label font-semibold text-lg mt-4 ">
+              Enter your task here
+            </label>
+            <input
+              onChange={(e) =>
+                edit ? setValue(e.target.value) : setTask(e.target.value)
+              }
+              type="text"
+              value={edit ? value : task}
+              className="input bg-[#ffffff42] w-full my-3"
+              placeholder="text"
+            />
+            {data.length > 0 && (
+              <div className=" flex justify-between items-center pr-5">
+                <div
+                  onClick={handleAllData}
+                  className="flex items-center gap-2 py-1 px-2 bg-[#000000] border border-[#ea1818] rounded-[5px] w-30 justify-center text-[#ca0a0a] text-[14px]  cursor-pointer"
+                >
+                  All data <FaTrashCan />
                 </div>
-              </li>
-            ))}
-          </ul>
-          {edit ? (
-            <button onClick={handleUpdate} className="btn btn-neutral mt-4">
-              Update
-            </button>
-          ) : (
-            <button onClick={handleClick} className="btn btn-neutral mt-4">
-              Submit
-            </button>
-          )}
-        </fieldset>
-      </form>
-    </div>
+                <div>
+                  {edit ? (
+                    <FaArrowRotateLeft
+                      className=" text-[#07c9ff] text-lg  cursor-pointer  "
+                      onClick={handleUpdate}
+                    />
+                  ) : (
+                    <FaLocationArrow
+                      className=" text-[#07c9ff] text-lg  cursor-pointer "
+                      onClick={handleClick}
+                    />
+                  )}
+                </div>
+              </div>
+            )}
+            <ul className=" max-h-[150px] overflow-y-auto ">
+              {data.map((item, index) => (
+                <li
+                  key={index}
+                  className="  w-full my-3 py-3 px-5  rounded-[5px] flex justify-between items-center text-lg bg-[#ffffff36] "
+                >
+                  <div
+                    className={`flex items-center gap-3 ${line ? "line-through" : "  "} `}
+                  >
+                    <input onClick={handleThrough}  className=" cursor-pointer " type="checkbox" />
+                    {item.value.todoName}
+                  </div>
+
+                  <div className="flex gap-3 items-center">
+                    <FaRegEdit
+                      onClick={() => handleEdit(item.value.todoName, item.id)}
+                      className="text-green-500 cursor-pointer "
+                    />
+
+                    <FaTrashCan
+                      onClick={() => handleDelete(item.id)}
+                      className=" cursor-pointer text-red-500 "
+                    />
+                  </div>
+                </li>
+              ))}
+            </ul>
+            {edit ? (
+              <button onClick={handleUpdate} className="btn btn-neutral mt-4">
+                Update
+              </button>
+            ) : (
+              <button onClick={handleClick} className="btn btn-neutral mt-4">
+                Submit
+              </button>
+            )}
+          </fieldset>
+        </form>
+      </div>
+    </>
   );
 };
 
